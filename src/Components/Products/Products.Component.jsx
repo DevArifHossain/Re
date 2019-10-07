116d70fb1b6f0ccfa40837131eed6c15ca8cb6a4
Arif Hossain <devarifhossain@gmail.com>
2019-10-07 12:35:00 +0600

    removed the patination plan

--- src/Components/Products/Products.Component.jsx
+++ src/Components/Products/Products.Component.jsx
@@ -23,51 +23,20 @@ const speProducts = products => {
   let urlPrams = window.location.pathname.substr(1);
   let categorizedProducts = products[urlPrams];
 
-  let currentPage = 1;
-  const productsPerPage = 10;
-
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentPosts = categorizedProducts.slice(
-    indexOfFirstProduct,
-    indexOfLastProduct
-  );
-
-  const totalPages = Math.ceil(categorizedProducts.length / productsPerPage);
-
-  let activePageee = 1;
-  const onChange = (e, { activePage }) => {
-    activePageee = activePage;
-  };
-
-  console.log(activePageee);
-
-  return (
-    <Container>
-      <div className="products-con">
-        {currentPosts.map(product => (
-          <ProductItem key={product.id} data={product} />
-        ))}
-      </div>
-      <Pagination
-        activePage={activePageee}
-        onPageChange={onChange}
-        totalPages={totalPages}
-        ellipsisItem={null}
-      />
-    </Container>
-  );
+  return categorizedProducts.map(product => (
+    <ProductItem key={product.id} data={product} />
+  ));
 };
 
 const Products = ({ products }) => {
   return (
-    <>
-      {/* <div className="products-con"> */}
-      {window.location.pathname === "/"
-        ? allProducts(products)
-        : speProducts(products)}
-      {/* </div> */}
-    </>
+    <Container>
+      <div className="products-con">
+        {window.location.pathname === "/"
+          ? allProducts(products)
+          : speProducts(products)}
+      </div>
+    </Container>
   );
 };
 
